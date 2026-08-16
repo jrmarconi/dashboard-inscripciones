4ef45a446de05a633b5adb16029e6f64b76c94ec
jrmarconi <jrmarconi.curso@gmail.com>
2026-08-15 23:47:49 -0300

    Update DashboardInscripciones.jsx

--- src/DashboardInscripciones.jsx
+++ src/DashboardInscripciones.jsx
@@ -489,7 +489,6 @@ export default function DashboardInscripciones() {
         if (rawActividad.includes('(CL_')) tipoOferta = 'Capacitación Laboral';
         else if (rawActividad.includes('(CT_')) tipoOferta = 'Curso';
         else if (rawActividad.includes('(TR_')) tipoOferta = 'Trayecto';
-        else if (rawActividad.includes('(MC_')) tipoOferta = 'Microcredencial';
 
         item.genero = inferGender(item.alumno);
         item.tipoOferta = tipoOferta;
@@ -718,7 +717,7 @@ export default function DashboardInscripciones() {
       }
 
       // Lógica para Actividades Iniciales, Cap. Laborales y Cursos
-      const isCapOrCurso = item.tipoOferta === 'Capacitación Laboral' || item.tipoOferta === 'Curso' || item.tipoOferta === 'Microcredencial';
+      const isCapOrCurso = item.tipoOferta === 'Capacitación Laboral' || item.tipoOferta === 'Curso';
       const actClean = cleanActivityName(item.actividad);
       
       // Chequeo usando el Set normalizado
